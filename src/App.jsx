a2e48fd95ad2ac406035ae55b2331cc289ade9d8
Deepanjan Ghosh <deepanjan@Deepanjans-MacBook-Air.local>
2021-04-27 21:18:15 +0530

    webpack configuration for procustion and development added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
     return (
         <div>
             <h1>Welcome to React application</h1>
+            <h2>{`Active Env: ${process.env.NODE_ENV}`}</h2>
         </div>
     )
 }
